004a3cc22b16c81e3560fda0a1ea924483b52ff3
Sascha Peilicke <sascha@peilicke.de>
2022-04-26 13:33:39 +0200

    Update Kotlin to 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
     id("com.diffplug.spotless") version "6.3.0"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
