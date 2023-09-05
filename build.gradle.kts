e680d6f270776de2699d009560df5992d4537ef4
Sascha Peilicke <sascha@peilicke.de>
2023-09-05 13:51:24 +0200

    Gradle: Update versions plugin to 0.47.0
    See https://github.com/ben-manes/gradle-versions-plugin/releases/tag/v0.47.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.10"
     id("com.android.library") version "7.2.2" apply false
     id("com.diffplug.spotless") version "6.21.0"
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 spotless {
@@ -15,14 +15,5 @@ spotless {
     }
     kotlinGradle {
         ktlint()
-    }
-}
-
-tasks {
-    withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
-        rejectVersionIf {
-            fun isStable(version: String) = Regex("^[0-9,.v-]+(-r)?$").matches(version)
-            !isStable(candidate.version) && isStable(currentVersion)
-        }
     }
 }
