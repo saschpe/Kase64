3ef64ef87e7fa8eb8e73202e4be9faae16364f53
Sascha Peilicke <sascha@peilicke.de>
2023-09-05 13:50:05 +0200

    Gradle: Update Spotless plugin to 6.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,20 @@
 plugins {
-    kotlin("jvm") version "1.9.10" apply false
+    kotlin("jvm") version "1.9.10"
     id("com.android.library") version "7.2.2" apply false
-    id("com.diffplug.spotless") version "6.9.0"
+    id("com.diffplug.spotless") version "6.21.0"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 spotless {
-    format("misc") {
-        target("**/*.gradle", "*.md", "**/.gitignore")
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
     freshmark {
-        target("*.md")
+        target("**/*.md")
         propertiesFile("gradle.properties")
     }
     kotlin {
-        target("source/**/src/**/*.kt")
-        targetExclude("**/build/**/*.kt")
-        ktlint().editorConfigOverride(
-            mapOf("disabled_rules" to "filename,no-wildcard-imports", "insert_final_newline" to false)
-        )
+        ktlint()
     }
     kotlinGradle {
-        target("**/*.gradle.kts")
-        ktlint().editorConfigOverride(mapOf("insert_final_newline" to false))
+        ktlint()
     }
 }
 
--- kase64/src/commonTest/kotlin/saschpe/kase64/Base64Test.kt
+++ kase64/src/commonTest/kotlin/saschpe/kase64/Base64Test.kt
@@ -27,9 +27,9 @@ class Base64Test {
             byteArrayOf(
                 -94, 124, -26, -112, -72, -84, 16, 11, 67, -45, 107, 38, -99, 79, 62, -49, 83, 26, -85, -70, -122, 53,
                 67, 42, -94, -87, 61, -74, 66, 0, 80, -125, -17, -11, -125, 63, 109, -15, 56, -95, -33, 18, 110, 47,
-                47, -20, -72, -34, 53, -69, 49, -45, 54, 53, -21, 43, 9, -84, -125, 72, -61, 76, 31, -46
+                47, -20, -72, -34, 53, -69, 49, -45, 54, 53, -21, 43, 9, -84, -125, 72, -61, 76, 31, -46,
             ),
-            "onzmkLisEAtD02smnU8+z1Maq7qGNUMqoqk9tkIAUIPv9YM/bfE4od8Sbi8v7LjeNbsx0zY16ysJrINIw0wf0g==".base64DecodedBytes
+            "onzmkLisEAtD02smnU8+z1Maq7qGNUMqoqk9tkIAUIPv9YM/bfE4od8Sbi8v7LjeNbsx0zY16ysJrINIw0wf0g==".base64DecodedBytes,
         )
     }
 
@@ -37,15 +37,15 @@ class Base64Test {
     fun byteArray_base64Encoded() {
         assertEquals(
             "xvrp9DBWlei2mG0ov9MN+A==", // value1
-            byteArrayOf(-58, -6, -23, -12, 48, 86, -107, -24, -74, -104, 109, 40, -65, -45, 13, -8).base64Encoded
+            byteArrayOf(-58, -6, -23, -12, 48, 86, -107, -24, -74, -104, 109, 40, -65, -45, 13, -8).base64Encoded,
         )
         assertEquals(
             "IkYJxF8nIQD9RY7Yk6r26A==", // value222
-            byteArrayOf(34, 70, 9, -60, 95, 39, 33, 0, -3, 69, -114, -40, -109, -86, -10, -24).base64Encoded
+            byteArrayOf(34, 70, 9, -60, 95, 39, 33, 0, -3, 69, -114, -40, -109, -86, -10, -24).base64Encoded,
         )
         assertEquals(
             "U0GeVBi2dNcdL2IO0nJo5Q==", // value555
-            byteArrayOf(83, 65, -98, 84, 24, -74, 116, -41, 29, 47, 98, 14, -46, 114, 104, -27).base64Encoded
+            byteArrayOf(83, 65, -98, 84, 24, -74, 116, -41, 29, 47, 98, 14, -46, 114, 104, -27).base64Encoded,
         )
     }
 
@@ -58,12 +58,12 @@ class Base64Test {
         assertEquals("Hello, world!", "SGVsbG8sIHdvcmxkIQ==".base64Decoded)
         assertEquals(
             Encoding.Standard.alphabet,
-            "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLw==".base64Decoded
+            "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLw==".base64Decoded,
         )
         assertEquals("abcd", "YWJjZA==".base64Decoded)
         assertEquals(
             "1234567890-=!@#\$%^&*()_+qwertyuiop[];'\\,./?><|\":}{P`~",
-            "MTIzNDU2Nzg5MC09IUAjJCVeJiooKV8rcXdlcnR5dWlvcFtdOydcLC4vPz48fCI6fXtQYH4=".base64Decoded
+            "MTIzNDU2Nzg5MC09IUAjJCVeJiooKV8rcXdlcnR5dWlvcFtdOydcLC4vPz48fCI6fXtQYH4=".base64Decoded,
         )
         assertEquals("saschpe", "c2FzY2hwZQ==".base64Decoded)
     }
@@ -78,12 +78,12 @@ class Base64Test {
         assertEquals("SGVsbG8sIHdvcmxkIQ==", "Hello, world!".encodeToByteArray().base64Encoded)
         assertEquals(
             "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLw==",
-            Encoding.Standard.alphabet.base64Encoded
+            Encoding.Standard.alphabet.base64Encoded,
         )
         assertEquals("YWJjZA==", "abcd".base64Encoded)
         assertEquals(
             "MTIzNDU2Nzg5MC09IUAjJCVeJiooKV8rcXdlcnR5dWlvcFtdOydcLC4vPz48fCI6fXtQYH4=",
-            "1234567890-=!@#\$%^&*()_+qwertyuiop[];'\\,./?><|\":}{P`~".base64Encoded
+            "1234567890-=!@#\$%^&*()_+qwertyuiop[];'\\,./?><|\":}{P`~".base64Encoded,
         )
         assertEquals("c2FzY2hwZQ==", "saschpe".base64Encoded)
     }
