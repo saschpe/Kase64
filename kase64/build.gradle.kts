4025efc4a3d7b01002420acc8b6e3ea66c13df1c
Sascha Peilicke <sascha@peilicke.de>
2025-06-19 14:07:19 +0200

    Gradle: Update Java toolchain version to 21

--- kase64/build.gradle.kts
+++ kase64/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
     }
 }
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 
 android {
     namespace = "saschpe.kase64"
