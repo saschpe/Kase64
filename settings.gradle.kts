a356d87fa59f5c41ca0a7ab080dc4a1a41c9de69
Sascha Peilicke <sascha@peilicke.de>
2025-06-26 09:00:15 +0200

    Gradle: Bump Android library plugin version to 8.11.0
    See: https://developer.android.com/build/releases/gradle-plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 
     plugins {
         kotlin("multiplatform") version "2.1.21"
-        id("com.android.library") version "8.10.1"
+        id("com.android.library") version "8.11.0"
         id("org.jetbrains.dokka") version "2.0.0"
     }
 }
