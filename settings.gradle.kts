3463c7cfcf60e624dbb31f26a42dad29ee8ae556
Sascha Peilicke <sascha@peilicke.de>
2026-06-13 12:16:16 +0200

    Gradle: Update kotlin multiplatform to 2.4.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("multiplatform") version "2.2.21"
+        kotlin("multiplatform") version "2.4.0"
         id("com.android.library") version "8.12.3"
         id("org.jetbrains.dokka") version "2.2.0"
     }
